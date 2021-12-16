5b607eb667b0aa0d60aebab1a52156e37464e019
_caio.cp_ <93060384+CaioCesarP@users.noreply.github.com>
2021-12-16 12:08:21 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,7 +12,19 @@ for (var i = 0; i < listaDeTeclas.length; i++) {
 
     const audio = `#som_${instrumento}`;
 
+    //Clicar em qualquer uma das teclas da tabela de button vai ativar o audio, pegando pelo ID
     tecla.onclick = function () {
         tocaSom(audio);
     };
+
+    //css 'ativa' acionado pelo botão 'Enter' ou 'Space'
+    tecla.onkeydown = function(evento) {
+        if(evento.code == "Enter"|| evento.code == "Space") {
+            tecla.classList.add('input[type=submit]');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('input[type=submit] || input[type=button]');
+    }
 }
